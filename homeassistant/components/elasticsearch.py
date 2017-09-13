1a0dea33ab68a9ec258d2813b61e537d4a2aa5e8
David Chauvière <d_chauviere@yahoo.fr>
2017-09-13 22:41:00 +0200

    Use elasticsearch python library

--- homeassistant/components/elasticsearch.py
+++ homeassistant/components/elasticsearch.py
@@ -12,55 +12,72 @@ import requests
 import voluptuous as vol
 
 from homeassistant.const import (
+    CONF_USERNAME, CONF_PASSWORD,
     CONF_URL, EVENT_HOMEASSISTANT_START,
     EVENT_HOMEASSISTANT_STOP, EVENT_STATE_CHANGED)
 from homeassistant.helpers import state
 import homeassistant.helpers.config_validation as cv
 
+REQUIREMENTS = ['elasticsearch==5.4.0']
+
 _LOGGER = logging.getLogger(__name__)
 
-DEFAULT_URL = 'http://localhost:9200/ha/states'
 DOMAIN = 'elasticsearch'
+CONF_ESINDICE = 'indice'
+CONF_ESTYPE = 'type'
 
 CONFIG_SCHEMA = vol.Schema({
     DOMAIN: vol.Schema({
-        vol.Optional(CONF_URL, default=DEFAULT_URL): cv.string,
+        vol.Optional(CONF_URL, default=['http://localhost:9200']):
+            vol.All(cv.ensure_list, [cv.string]),
+        vol.Optional(CONF_ESINDICE, default='hass'): cv.string,
+        vol.Optional(CONF_ESTYPE, default='event'): cv.string,
+        vol.Optional(CONF_USERNAME): cv.string,
+        vol.Optional(CONF_PASSWORD, default=''): cv.string,
     }),
 }, extra=vol.ALLOW_EXTRA)
 
 
 def setup(hass, config):
     """Setup the Elasticsearch feeder."""
+    from elasticsearch import Elasticsearch
     conf = config[DOMAIN]
-    url = conf.get(CONF_URL)
+    es_args = {}
+    if CONF_USERNAME in conf:
+        es_args['http_auth'] = (conf[CONF_USERNAME], conf[CONF_PASSWORD])
 
-    response = requests.post(url, timeout=10)
-    # if response.status_code in (200, 201, 404):
-    #     _LOGGER.debug('Connection to Elasticsearch possible')
-    # else:
-    #     _LOGGER.error('Not able to connect to Elasticsearch')
-    #     return False
+    es_logger = logging.getLogger('elasticsearch')
+    es_logger.propagate = True
+    es_logger.setLevel(logging.INFO)
+    try:
+        es_client = Elasticsearch(conf.get(CONF_URL), **es_args)
+    except Exception:
+        _LOGGER.error('Not able to connect to Elasticsearch', exc_info=False)
+        return False
 
-    ElasticsearchFeeder(hass, url)
+    ElasticsearchFeeder(hass, es_client, conf.get(CONF_ESINDICE), conf.get(CONF_ESTYPE))
     return True
 
 
 class ElasticsearchFeeder(threading.Thread):
-    """Feed data to Graphite."""
+    """Feed data to Elasticsearch."""
 
-    def __init__(self, hass, url):
+    def __init__(self, hass, es_client, es_indice, es_type):
         """Initialize the feeder."""
         super(ElasticsearchFeeder, self).__init__(daemon=True)
+
         self._hass = hass
-        self._url = url
         self._queue = queue.Queue()
         self._quit_object = object()
         self._we_started = False
+        self._es_client = es_client
+        self.es_indice = es_indice
+        self.es_type = es_type
 
         hass.bus.listen_once(EVENT_HOMEASSISTANT_START, self.start_listen)
         hass.bus.listen_once(EVENT_HOMEASSISTANT_STOP, self.shutdown)
         hass.bus.listen(EVENT_STATE_CHANGED, self.event_listener)
-        _LOGGER.debug('Elasticsearch feeding to %s initialized', self._url)
+        _LOGGER.debug('Elasticsearch feeding initialized')
 
     def start_listen(self, event):
         """Start event-processing thread."""
@@ -82,12 +99,6 @@ class ElasticsearchFeeder(threading.Thread):
             _LOGGER.error('Elasticsearch feeder thread has died, not '
                           'queuing event!')
 
-    def _send_to_elasticsearch(self, data):
-        """Send data to Elasticsearch."""
-        response = requests.post(self._url, data=data, timeout=10)
-        if response.status_code not in (200, 201):
-            _LOGGER.error('Not able to send to Elasticsearch')
-
     def _report_attributes(self, entity_id, new_state):
         """Report the attributes."""
         now = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%f%z')
@@ -102,7 +113,10 @@ class ElasticsearchFeeder(threading.Thread):
                             'attributes': things })
 
         _LOGGER.debug('Sending to Elasticsearch: %s', data)
-        self._send_to_elasticsearch(data)
+        try:
+            self._es_client.index(index=self.es_indice, doc_type=self.es_type, body=data)
+        except Exception:
+            _LOGGER.error('Not able to send to Elasticsearch')
 
     def run(self):
         """Run the process to export the data."""
